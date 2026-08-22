5f3e2ca16123076b61156bc22fdb7a27f038b14b
Rakesh-88-dev <rakeshrath2003@gmail.com>
2026-08-22 10:40:03 +0530

    Recursion and back tracking

--- Recursion/02_RecursionPractice.py
+++ Recursion/02_RecursionPractice.py
@@ -21,11 +21,33 @@
 
 # Print number linearly from n to 1
 
-def printNumber(i,n):
-    if i < n:
-        return
-    print(i)
-    printNumber(i-1,n)
-if __name__ == "__main__":
-    printNumber(5,1)    
+# def printNumber(i,n):
+#     if i < n:
+#         return
+#     print(i)
+#     printNumber(i-1,n)
+# if __name__ == "__main__":
+#     printNumber(5,1)    
         
+# Back Tracking
+# print 1 to n linearly using back tracking
+
+# def printNumber(i,n):
+#     if i < n:
+#         return
+#     printNumber(i-1,n)
+#     print(i)
+    
+# if __name__ == "__main__":
+#     printNumber(5,1)    
+
+#print n to 1 using back tracking
+
+def printNumber(i,n):
+    if i >n:
+        return
+    printNumber(i+1,n)
+    print(i)
+
+if __name__ == "__main__":
+    printNumber(1,5)       
