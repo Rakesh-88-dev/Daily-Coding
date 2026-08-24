46f646647059f92d9bcdbca3a69f885b5414ff67
Rakesh-88-dev <rakeshrath2003@gmail.com>
2026-08-24 17:02:51 +0530

    Array Reverse

--- Recursion/04_ReverseArray.py
+++ Recursion/04_ReverseArray.py
@@ -24,3 +24,21 @@ arr = [1, 2, 3, 4, 5]
 reverseArray(arr, 0, len(arr) - 1)
 
 print(arr)
+
+
+def reverseArray(arr, i, n):
+    if i >= n // 2:
+        return
+
+    arr[i], arr[n - i - 1] = arr[n - i - 1], arr[i]
+
+    reverseArray(arr, i + 1, n)
+
+
+if __name__ == "__main__":
+    arr = [1, 2, 3, 4, 5]
+    n = len(arr)
+
+    reverseArray(arr, 0, n)
+
+    print(arr)
